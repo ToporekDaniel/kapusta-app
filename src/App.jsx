ebf5128c5d35744aadd1eec49f90e4436445301b
ToporekDaniel <danieltoporek0@gmail.com>
2024-05-08 20:05:00 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Mam nadzieję że tutaj powstanie nasza wspaniała aplikacja do liczenia
+        kapusty ;-)
+      </h1>
+    </>
+  );
+}
+
+export default App;
