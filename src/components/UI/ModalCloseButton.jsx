50d467982feee2f12920de289b605f16caad500a
amending1 <joasia.sowiak@gmail.com>
2024-05-10 20:05:32 +0200

    stworzenie komponentu ModalCloseButton
    W Figmie są dwie wersje modala wyjścia ( z pytaniami 1. Do you really want to leave?
     2. Are you sure?). Nie wiem, gdzie się który powinien pojawić. Stworzyłam komponent z propsem {title} do zmiany tego pytania właśnie

---
+++ src/components/UI/ModalCloseButton.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import styles from "../css/ModalCloseButton.module.css";
+
+// w propsie title powinny być dwie wartości: 
+// 1. Do you really want to leave?
+// 2. Are you sure?
+function ModalCloseButton({ title }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const history = useHistory();
+
+  const handleYesClick = () => {
+    history.push("/login"); //należy podać właściwą ścieżkę (!!!)
+  };
+
+  const handleNoClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div
+      className={styles.ModalWrapper}
+      style={{ display: isOpen ? "block" : "none" }}
+    >
+      <div className={styles.ModalContainer}>
+        <div className={styles.ModalTitle}>
+          <p>{title}</p>
+        </div>
+        <div className={styles.ModalButtons}>
+          <button className={styles.ModalButtonStyle} onClick={handleYesClick}>
+            YES
+          </button>
+          <button className={styles.ModalButtonStyle} onClick={handleNoClick}>
+            NO
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalCloseButton;
