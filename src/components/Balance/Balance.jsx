e6fb95bb60e544a8b26b70e31e12e904929818b1
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-10 21:55:26 +0200

    balance component

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,34 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { disableInput } from '../../app/store'
 import { useFinance } from '../../../contexts/FinanceContext';
-// import './Balance.css';
 
 function Balance() {
-  const [balance, setBalance] = useState(0);
   const [inputBalance, setInputBalance] = useState('');
-  const [showModal, setShowModal] = useState(false); 
-const { expenses, income } = useFinance()
+  const { expenses, income } = useFinance();
+  const inputDisabled = useSelector(state => state.balance.inputDisabled);
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     const totalExpenses = expenses.reduce((acc, expense) => acc + expense.amount, 0);
     const totalIncome = income.reduce((acc, income) => acc + income.amount, 0);
     const calculatedBalance = totalIncome - totalExpenses;
-    setBalance(calculatedBalance);
     setInputBalance(calculatedBalance.toFixed(2));
-    setShowModal(calculatedBalance === 0); 
   }, [expenses, income]);
 
   const handleConfirm = () => {
     if (parseFloat(inputBalance) === 0) {
       setShowModal(true);
     } else {
-      setBalance(parseFloat(inputBalance));
+      alert('Balance confirmed: ' + inputBalance + 'PLN');
+      dispatch(disableInput());
       setShowModal(false);
-      alert('Balance confirmed: ' + inputBalance + ' UAH');
     }
   };
 
   const handleInputChange = (e) => {
-    setInputBalance(e.target.value);
-    setShowModal(parseFloat(e.target.value) === 0);
+    const value = e.target.value;
+    if (!isNaN(value) && value.trim() !== '') {
+      setInputBalance(value);
+      setShowModal(false);
+    }
   };
 
   const closeModal = () => {
@@ -43,19 +47,22 @@ const { expenses, income } = useFinance()
         className="balance-value"
         value={inputBalance}
         onChange={handleInputChange}
+        disabled={inputDisabled}
       />
       {showModal && (
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={closeModal}>&times;</span>
-            <p>Hello! To get started, enter the current balance of your account!!</p>
+            <p>Hello! To get started, enter the current balance of your account!</p>
             <p>You can't spend money until you have it :)</p>
           </div>
         </div>
       )}
-      <button className="confirm-button" onClick={handleConfirm} disabled={parseFloat(inputBalance) === 0}>
-        CONFIRM
-      </button>
+      {!inputDisabled && (
+        <button className="confirm-button" onClick={handleConfirm}>
+          CONFIRM
+        </button>
+      )}
     </div>
   );
 }
