79aa3408c2860b1a94d52f24bec67745b4235145
amending1 <joasia.sowiak@gmail.com>
2024-05-11 21:02:13 +0200

    podpięcie komponentu

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Balance from '../../components/Balance/Balance'; 
 import Reports from '../Reports/Reports';
+import SummaryTable from '../../components/SummaryTable';
+
 function Dashboard() {
   return (
     <div>
       <Balance />
       <Reports />
+      <SummaryTable />
     </div>
   );
 }
