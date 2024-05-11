680fa090503a6ed559cf199aa1774d3c898641a6
amending1 <joasia.sowiak@gmail.com>
2024-05-11 21:22:37 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Balance from '../../components/Balance/Balance'; 
 import Reports from '../Reports/Reports';
-import SummaryTable from '../../components/SummaryTable';
+import SummaryTable from '../../components/SummaryTable/SummaryTable.jsx';
 
 function Dashboard() {
   return (
