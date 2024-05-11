92506f4cf72484d1426df79bf4a73c26b91eb895
amending1 <joasia.sowiak@gmail.com>
2024-05-11 15:40:17 +0200

    stworzenie komponentu FormTransaction
    proszę nie sprawdzać jeszcze, do mocnego dopracowania

---
+++ src/components/FormTransaction/FormTransaction.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "./FormTransaction.modules.css";
+
+function FormTransaction() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <form className={styles.ContainerTransaction}>
+      <div className={styles.DatapickerWrapper}>
+        <svg></svg>
+        <div className={styles.DatapickerInputContainer}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+      </div>
+
+      <label className={styles.TransactionDescription}>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Product description"
+        />
+      </label>
+
+      <div className={styles.TransactionSelect}>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Product category</option>
+          <option value="Transport">Transport</option>
+          <option value="Products">Products</option>
+          <option value="Health">Health</option>
+          <option value="Alcohol">Alcohol</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Housing">Housing</option>
+          <option value="Technique">Technique</option>
+          <option value="Communal, communication">
+            Communal, communication
+          </option>
+          <option value="Eduction">Eduction</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>
+
+      <label>
+        <input
+          className={styles.CalcInput}
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </label>
+    </form>
+  );
+}
+
+export default FormTransaction;
