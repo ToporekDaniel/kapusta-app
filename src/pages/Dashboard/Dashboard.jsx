87a5ec15f1ef54ccf46f20f910d60e7596f2e8a6
amending1 <joasia.sowiak@gmail.com>
2024-05-11 21:04:11 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Balance from '../../components/Balance/Balance'; 
 import Reports from '../Reports/Reports';
+import FormTransaction from '../../components/FormTransaction/FormTransaction.jsx'; 
 function Dashboard() {
   return (
     <div>
       <Balance />
       <Reports />
+      <FormTransaction />
     </div>
   );
 }
