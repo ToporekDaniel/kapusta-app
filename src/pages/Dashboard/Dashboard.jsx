5dda6d0ff1b120e3f9a274a1f24c21923ddc67d7
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-12 12:43:34 +0200

    stylowanie balance - mam problem z svg i uzywaniem zmiennych z index.css

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Balance from '../../components/Balance/Balance'; 
 import Reports from '../Reports/Reports';
+import './Dashboard.css';
 function Dashboard() {
   return (
-    <div>
+    <div className='dashboard'>
       <Balance />
       <Reports />
     </div>
--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
+import './Reports.css'; 
 
 function Reports() {
   return (
-    <div className="reports">
-    <Link to="/reports">reports</Link> 
-       </div>
+    <div className="reports-container">
+      <Link to="/reports" className="reports-link">
+        <p>        Reports 
+</p>
+        <svg className="icon-reports" viewBox="0 0 32 32" fill="gray">
+          <use href="/icons.svg#icon-reports" /> 
+        </svg>
+      </Link>
+    </div>
   );
 }
 
