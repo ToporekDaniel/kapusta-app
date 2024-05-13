4e1a84f738f4195f5d4e2cf2414d36b1d6c6216f
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-13 16:44:02 +0200

    fix

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Dashboard from "../../pages/Dashboard/Dashboard";
+import Hero from "../Hero/Hero";
+
+const HomePage = () => {
+    return (
+      <>
+              <Dashboard />
+
+        <Hero />
+      </>
+    );
+  }
+  
+  export default HomePage;
