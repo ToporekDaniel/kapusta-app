dd00186fb72e54baa8162e6a3bff72277bd9a913
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-05-14 22:04:48 +0200

    up

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -1,16 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import FormTransaction from '../components/FormTransaction/FormTransaction';
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: {
-        balance: 'Balance',
+        balance: 'Balance:',
+        firstBalanceP: 'Hello! To get started, enter the current balance of your account!',
+        secondBalanceP: "You can't spend money until you have it :)",
+        FormTransactionPlaceholder: 'Product description',
+        ProductCategory: "Product category",
+        Transport: 'Transport',
+        Products: 'Products',
       },
     },
     pl: {
       translation: {
-        balance: 'Balans',
+        balance: 'Balans:',
+        firstBalanceP: 'Cześć! Aby rozpocząć, podaj aktualny stan swojego konta!',
+        secondBalanceP: "Nie możesz wydawać pieniędzy, dopóki ich nie masz :)",
+        FormTransactionPlaceholder: 'Opis produktu',
+        ProductCategory: "Kategoria produktu",
+        Transport: 'Transport',
+        Products: 'Products',
       },
     },
   },
