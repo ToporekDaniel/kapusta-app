5764e4518f67ad9698535437da20d92595d5c8c1
Arkadiusz Daugielewicz <akson_control@o2.pl>
2024-05-14 03:58:12 +0200

    feat: add LoginPage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import Dashboard from "../../pages/Dashboard/Dashboard";
 import Hero from "../Hero/Hero";
+import { LoginForm } from "../LoginGoogle/LoginPage";
 
 const HomePage = () => {
   return (
@@ -7,6 +8,7 @@ const HomePage = () => {
       <Dashboard />
 
       <Hero />
+      <LoginForm />
     </>
   );
 };
