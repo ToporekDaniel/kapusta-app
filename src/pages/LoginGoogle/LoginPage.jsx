5a193e0f87f1ddbcd88191b49db914ec517620a0
ToporekDaniel <danieltoporek0@gmail.com>
2024-05-19 23:35:22 +0200

    poprawa stylizacji1

--- src/pages/LoginGoogle/LoginPage.jsx
+++ src/pages/LoginGoogle/LoginPage.jsx
@@ -4,6 +4,10 @@ import css from "./LoginPage.module.css";
 const LoginPage = () => {
   return (
     <div className={css.login}>
+      <div className={css.logo}>
+        <h1 className={css.title}>Kapu$ta</h1>
+        <p className={css.subtitle}>Your personal finance manager</p>
+      </div>
       <LoginForm className={css.form} />
     </div>
   );
