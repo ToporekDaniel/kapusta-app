b8fc1c445f9df9ec1c450581fcf7c56a7d3126e2
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-21 01:59:05 +0200

    kombinowania z chartami ciag dalszy

--- src/components/ExpensesCategories/ExpensesCategories.jsx
+++ src/components/ExpensesCategories/ExpensesCategories.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './ExpensesCategories.module.css';
 import ProductsIcon from '../../assets/icons/products.svg?react';
--- src/pages/ReportsChart/ReportsChart.jsx
+++ src/pages/ReportsChart/ReportsChart.jsx
@@ -1,57 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useFinance } from "../../../contexts/FinanceContext";
 import ExpensesChart from "../../components/ExpensesChart/ExpensesChart";
 import ExpensesCategories from "../../components/ExpensesCategories/ExpensesCategories";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import css from "./ReportsChart.module.css";
-import style from "../HomePage/HomePage.module.css";
 
 function ReportsChart() {
   const { expenses, income } = useFinance();
-  const [selectedCategory, setSelectedCategory] = useState(null);
   const [chartData, setChartData] = useState([]);
+  const { categoryName } = useParams();
 
   useEffect(() => {
-    if (selectedCategory) {
-      const data = expenses
-        .filter((exp) => exp.category === selectedCategory)
-        .map((exp) => ({
-          category: exp.category,
-          amount: exp.amount,
-        }));
-      setChartData(data);
+    console.log("Full Expenses Data:", expenses); 
+    if (categoryName) {
+      const filteredData = expenses.filter((exp) => exp.category === categoryName);
+      console.log(`Data for category ${categoryName}:`, filteredData);
+      const chartData = filteredData.map((exp) => ({
+        category: exp.category,
+        amount: exp.amount,
+      }));
+      setChartData(chartData);
     } else {
       setChartData([]);
     }
-  }, [expenses, selectedCategory]);
+  }, [expenses, categoryName]);
 
   const totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
   const totalIncome = income.reduce((sum, item) => sum + item.amount, 0);
 
-  const handleCategorySelect = (category) => {
-    setSelectedCategory(category);
-  };
-
   return (
     <div className={css["reports-chart-container"]}>
-         <div className={style["background-top"]}></div>
-      <div className={style["background-bottom"]}></div>
-      <div className={css["reports-container"]}>
-        <Dashboard />
-        <div className={css["header-reports-total"]}>
-          <ul className={css["header-reports-list"]}>
-            <li className={css["header-reports-item"]}>
-              Expenses:<span className={css["text-red"]}>{totalExpenses}</span>
-            </li>
-           <div className={css["header-reports-div"]}></div>
-            <li className={css["header-reports-item"]}>
-              Incomes:<span className={css["text-green"]}>{totalIncome}</span>
-            </li>
-          </ul>
-        </div>
-        <ExpensesCategories onCategorySelect={handleCategorySelect} />
+      <Dashboard />
+      <div>
+        <p>Expenses: {totalExpenses}</p>
+        <p>Incomes: {totalIncome}</p>
       </div>
-      {selectedCategory && <ExpensesChart data={chartData} />}
+      <ExpensesCategories />
+      {categoryName && <ExpensesChart data={chartData} />}
     </div>
   );
 }
