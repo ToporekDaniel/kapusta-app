99582574a7eaed18793a3cf3840b3548eae65cc8
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-21 15:30:22 +0200

    income dziala

--- src/components/IncomeReport/IncomeReport.jsx
+++ src/components/IncomeReport/IncomeReport.jsx
@@ -1,37 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, NavLink } from 'react-router-dom';
 import { useFinance } from '../../../contexts/FinanceContext';
-import IncomeChart from '../../components/IncomeChart/IncomeChart'; 
+import IncomeChart from '../../components/IncomeChart/IncomeChart';
 import css from './IncomeReport.module.css';
+import SalaryIcon from '../../assets/icons/salary.svg?react';
+import AddIncomeIcon from '../../assets/icons/addIncome.svg?react';
 
-function IncomeReport() {
-  const { income } = useFinance();
-  const [chartData, setChartData] = useState([]);
-  const { categoryName } = useParams(); 
+function IncomeReports() {
+    const { incomes } = useFinance();
+    const [chartData, setChartData] = useState([]);
+    const { categoryName } = useParams();
 
-  useEffect(() => {
-    if (categoryName) {
-      const filteredData = income.filter(inc => inc.category === categoryName);
-      const dataForChart = filteredData.map(inc => ({
-        category: inc.category,
-        amount: inc.amount,
-      }));
-      setChartData(dataForChart);
-    } else {
-      setChartData([]);
-    }
-  }, [income, categoryName]);
+    const categories = [
+        { name: 'Salary', Icon: SalaryIcon, quantity: 45000 },
+        { name: 'Add. Income', Icon: AddIncomeIcon, quantity: 1500 },
+    ];
 
-  return (
-    <div className={css['income-report']}>
-      <h1>{categoryName} Income</h1>
-      {chartData.length > 0 ? (
-        <IncomeChart data={chartData} />
-      ) : (
-        <p>No data available for this category.</p>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        if (categoryName) {
+            const filteredData = incomes.filter(inc => inc.category === categoryName);
+            setChartData(filteredData.map(inc => ({
+                category: inc.category,
+                amount: inc.amount,
+            })));
+        } else {
+            setChartData([]);
+        }
+    }, [incomes, categoryName]);
+
+    return (
+        <div className={css['income-report-wrapper']}>
+            <div className={css['income-report']}>
+                <ul className={css['products-container']}>
+                    {categories.map((category) => (
+                        <li key={category.name} className={css['product-card']}>
+                            <NavLink
+                                to={`/reports/income/${category.name}`}
+                                className={({ isActive }) => isActive ? `${css['active']} ${css['category']}` : css['category']}
+                            >
+                                <p className={css['text']}>{category.quantity}</p>
+                                {category.Icon && <category.Icon className={css['icon']} />}
+                                <span className={css['text']}>{category.name}</span>
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+                <div>
+                    {chartData.length > 0 ? (
+                        <IncomeChart data={chartData} />
+                    ) : (
+                        <p>No data available for this category.</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default IncomeReport;
+export default IncomeReports;
