c0b067a56365bcbdbc36cc98c0b8545498094bd7
Daniel Toporek <138520262+ToporekDaniel@users.noreply.github.com>
2024-05-21 15:15:19 +0200

    Revert "fix: login oAuth 2.o - done"

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./App.jsx";
@@ -7,9 +6,7 @@ import i18n from "../src/app/i18n.js"
 import { I18nextProvider } from 'react-i18next';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="835299169883-3ek9rfaqabfh13c39tm23db8qlp56o7c.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
