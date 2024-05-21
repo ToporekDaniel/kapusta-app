5a3d564718e2646105b07746f986ce7f969e96a2
ToporekDaniel <danieltoporek0@gmail.com>
2024-05-21 15:05:13 +0200

    poprawa url i zaciągnięcie navigacji w raportach

--- src/pages/ReportsChart/ReportsChart.jsx
+++ src/pages/ReportsChart/ReportsChart.jsx
@@ -1,51 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { useFinance } from '../../../contexts/FinanceContext'; 
-import ExpensesChart from '../../components/ExpensesChart/ExpensesChart';
-import ExpensesCategories from '../../components/ExpensesCategories/ExpensesCategories';
-import Dashboard from '../../components/Dashboard/Dashboard';
-import css from './ReportsChart.module.css';
+import React, { useEffect, useState } from "react";
+import { useFinance } from "../../../contexts/FinanceContext";
+import ExpensesChart from "../../components/ExpensesChart/ExpensesChart";
+import ExpensesCategories from "../../components/ExpensesCategories/ExpensesCategories";
+import Dashboard from "../../components/Dashboard/Dashboard";
+import css from "./ReportsChart.module.css";
 import { useUser } from "./../../lib/customHooks";
+import { useNavigate } from "react-router-dom";
+import { APP_ROUTES } from "../../utils/constants";
 
 function ReportsChart() {
-
   const navigate = useNavigate();
   const { user, authenticated } = useUser();
   if (!user || !authenticated) {
     navigate(APP_ROUTES.SIGN_IN);
-  } const { expenses, income } = useFinance(); const [selectedCategory, setSelectedCategory] = useState(null);
-    const [chartData, setChartData] = useState([]);
+  }
+  const { expenses, income } = useFinance();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [chartData, setChartData] = useState([]);
 
-    useEffect(() => { if (selectedCategory) {
-            const data = expenses.filter(exp => exp.category === selectedCategory).map(exp => ({
-                category: exp.category,
-                amount: exp.amount
-            }));
-            setChartData(data);
-        } else {
-            setChartData([]);
-        }
-    }, [expenses, selectedCategory]);
+  useEffect(() => {
+    if (selectedCategory) {
+      const data = expenses
+        .filter((exp) => exp.category === selectedCategory)
+        .map((exp) => ({
+          category: exp.category,
+          amount: exp.amount,
+        }));
+      setChartData(data);
+    } else {
+      setChartData([]);
+    }
+  }, [expenses, selectedCategory]);
 
-    const totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
-    const totalIncome = income.reduce((sum, item) => sum + item.amount, 0);
+  const totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
+  const totalIncome = income.reduce((sum, item) => sum + item.amount, 0);
 
-    const handleCategorySelect = (category) => {
-        setSelectedCategory(category);
-    };
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
 
-    return (
-        <div className={css['reports-chart-container']}>
-            <div className={css['reports-container']}>
-                <Dashboard />
-                <div className={css['header-reports-chart']}>
-                    <p>Expenses: {totalExpenses}</p>
-                    <p>Incomes: {totalIncome}</p>
-                </div>
-                <ExpensesCategories onCategorySelect={handleCategorySelect} />
-            </div>
-            {selectedCategory && <ExpensesChart data={chartData} />}
+  return (
+    <div className={css["reports-chart-container"]}>
+      <div className={css["reports-container"]}>
+        <Dashboard />
+        <div className={css["header-reports-chart"]}>
+          <p>Expenses: {totalExpenses}</p>
+          <p>Incomes: {totalIncome}</p>
         </div>
-    );
+        <ExpensesCategories onCategorySelect={handleCategorySelect} />
+      </div>
+      {selectedCategory && <ExpensesChart data={chartData} />}
+    </div>
+  );
 }
 
 export default ReportsChart;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,12 @@
-const API_URL = 'http://localhost:3001'
+const API_URL = "https://kapusta-server.onrender.com";
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/register`,
   SIGN_IN: `${API_URL}/api/auth/login`,
   GET_USER: `${API_URL}/api/user/`,
-}
+};
 
 export const APP_ROUTES = {
-  HOME: '/',
-  SIGN_UP: '/signup',
-  SIGN_IN: '/signin',
-}
+  HOME: "/",
+  SIGN_UP: "/signup",
+  SIGN_IN: "/signin",
+};
