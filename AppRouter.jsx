2c34a6881758f8b44260eea54fab498c022f9c1f
Alicja Poltorak <alicjaxpoltorak@gmail.com>
2024-05-21 16:27:35 +0200

    new

--- AppRouter.jsx
+++ AppRouter.jsx
@@ -12,11 +12,10 @@ import ReportsChart from "./src/pages/ReportsChart/ReportsChart.jsx";
 import HeroExpenses from "./src/components/Hero/HeroExpenses";
 import HeroIncome from "./src/components/Hero/HeroIncome";
 import ExpensesReport from "./src/components/ExpensesReport/ExpensesReport.jsx";
-import IncomeReport from "./src/components/IncomeReport/IncomeReport.jsx";
+import IncomeReport from "./src/components/IncomeReport/IncomeReport.jsx";  // Ensure correct import
 
-import  LoginPage  from './src/pages/LoginGoogle/LoginPage.jsx';
+import LoginPage from './src/pages/LoginGoogle/LoginPage.jsx';
 import { APP_ROUTES } from "./src/utils/constants.js";
-
 
 const AppRouter = () => {
   return (
@@ -33,8 +32,8 @@ const AppRouter = () => {
           <Route path="/reports" element={<ReportsChart />}>
             <Route index element={<Navigate to="expenses" />} />
             <Route path="expenses" element={<ExpensesReport />} />
-            <Route path="income" element={<IncomeReport />} />
-            </Route>
+            <Route path="income" element={<IncomeReport />} /> 
+          </Route>
         </Routes>
       </Router>
     </FinanceProvider>
@@ -42,8 +41,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-
-
-
-
-
